419b4233aca4d9a20a10307d4545304aeeb2e7fa
CShingiro <cshingiro@sociallyradicalwebdesign.com>
2023-07-21 21:42:12 +0800

    Update to board to be made visible

--- board.py
+++ board.py
@@ -7,7 +7,12 @@ class Board:
         for i in range(board_length):
             self.board.append([])
             for j in range(board_height):
-                self.board[i].append([])
+                self.board[i][j] = 0
+        
     
     def __repr__(self):
-        return str(self.board)
+        return self.board
+
+
+test_board = Board(9,5)
+test_board
