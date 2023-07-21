37db67069f20df8a6f80e296dc21f91f450a5200
CShingiro <cshingiro@sociallyradicalwebdesign.com>
2023-07-21 11:54:00 +0800

    initial board and points set up

---
+++ board.py
@@ -0,0 +1,10 @@
+class Board:
+    def __init__(self, board_length,board_height):
+        self.board = []
+        for i in range(board_length):
+            self.board.append([])
+            for j in range(board_height):
+                self.board[i].append([])
+    
+    def __repr__(self):
+        return str(self.board)
---
+++ point.py
@@ -0,0 +1,7 @@
+class Point:
+    
+    def __init__(self,score):
+        self.score = score
+    
+    def __repr__(self) -> str:
+        return str(self.score)
